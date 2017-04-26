dedad1d3ac29f19d158dcbce0c1b6ac8824ecc02
Jan Phillip Carreon <janphillipcarreon@gmail.com>
2017-04-26 00:19:01 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,18 @@
+def my_collect(arr)
+  if block_given?
+    i = 0
+    result=[]
+    while i < arr.length
+      result<<yield(arr[i])
+      i = i + 1
+    end
+    result
+  else
+     "Hey! No block was given!"
+  end
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
